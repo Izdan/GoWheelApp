557d71fe29829c840afecc796d74c3a61bd77716
Izdan <fidanzrd97@gmail.com>
2022-01-26 23:46:04 +0800

    First Version of GoWheel

---
+++ src/main/java/com/izdan/GoWheel/GoWheelApplication.java
@@ -0,0 +1,13 @@
+package com.izdan.GoWheel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoWheelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoWheelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/izdan/GoWheel/GoWheelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izdan.GoWheel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoWheelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
